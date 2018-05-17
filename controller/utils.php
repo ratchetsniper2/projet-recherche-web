3e3620f6a72e234ab0221f5b2a37da566911abea
ratchetsniper2 <ratchetsniper2@gmail.com>
2018-05-17 10:30:22 +0200

    update utils script

--- controller/utils.php
+++ controller/utils.php
@@ -1,17 +1,20 @@
 <?php
 
 /**
- * @return array [fileName, content]
+ * @return array [fileName, [fileName, fileName]]
  */
 function getDocuments() {
     $documentFolder = "../model/documents";
 
-    $filesData = [];
+    $filesLink = [];
 
     $files = array_diff(scandir($documentFolder), array('..', '.'));
     foreach($files as $file) {
-        $filesData[$file] = file_get_contents($documentFolder . "/" . $file);
+        $content = file_get_contents($documentFolder . "/" . $file);
+
+        preg_match_all('~<a(.*?)href="([^"]+)"(.*?)>~', $content, $matches);
+        $filesLink[$file] = array_diff($matches[2], array($file));
     }
 
-    return $filesData;
+    return $filesLink;
 }
