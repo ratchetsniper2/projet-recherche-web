8fcf5e96a69063838b3d3459a4173c698422138f
ratchetsniper2 <ratchetsniper2@gmail.com>
2018-05-17 11:40:04 +0200

    update getFilesLinksData function

--- model/utils.php
+++ model/utils.php
@@ -1,19 +1,37 @@
 <?php
 
 /**
- * @return array [fileName, [linkFileName, linkFileName]]
+ * @return array ["linkTo" => [], "linkedFrom" => []]
  */
 function getFilesLinksData() {
     $documentFolder = "../model/documents";
 
-    $filesLink = [];
+    $filesLink["linkTo"] = [];
+    $filesLink["linkedFrom"] = [];
 
     $files = array_diff(scandir($documentFolder), array('..', '.'));
+
+    foreach($files as $file) {
+        $filesLink["linkTo"][$file] = [];
+        $filesLink["linkedFrom"][$file] = [];
+    }
+
     foreach($files as $file) {
         $content = file_get_contents($documentFolder . "/" . $file);
 
         preg_match_all('~<a(.*?)href="([^"]+)"(.*?)>~', $content, $matches);
-        $filesLink[$file] = array_diff($matches[2], array($file));
+
+        foreach ($matches[2] as $linkFile) {
+            if ($linkFile !== $file) {
+                if (array_search($linkFile, $filesLink["linkTo"][$file]) === false) {
+                    $filesLink["linkTo"][$file][] = $linkFile;
+                }
+
+                if (array_search($file, $filesLink["linkedFrom"][$linkFile]) === false) {
+                    $filesLink["linkedFrom"][$linkFile][] = $file;
+                }
+            }
+        }
     }
 
     return $filesLink;
